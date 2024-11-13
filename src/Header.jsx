36fb968511858b786aa9d1575c6ec6f45b1947a1
Web Developer <juniorwebdeveloper.mtkn@gmail.com>
2024-11-13 10:57:28 +0630

    adding nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,17 @@ import { useState } from "react";
 const Header = () => {
 	const [auth, setAuth] = useState(true);
 
+    const openMenu = (menu) => {
+        console.log(menu)
+    }
+
+	const closeMenu = (menu) => {
+		console.log(menu)
+	}
+
 	return (
-		<div className="fixed top-0 left-0 w-full py-4 hidden lg:block bg-white text-dark_blue_2 font-poppins shadow-lg d_container">
-			<div className="flex items-center justify-between gap-6">
+		<div className="fixed top-0 left-0 w-full hidden lg:block shadow-lg">
+			<div className="flex items-center justify-between gap-6 border-b d_container py-4">
 				<div>
 					<a href="/">DShop</a>
 				</div>
@@ -24,6 +32,18 @@ const Header = () => {
 					</div>
 				)}
 			</div>
+            <div>
+                <ul className="flex items-center justify-start gap-4 text-dark_blue_2 font-poppins d_container *:py-4 *:px-2 *:cursor-pointer [&>*:hover]:text-blue-500 *:border-b-4 rounded *:border-transparent [&>*:hover]:border-blue-500">
+                    <li>Home</li>
+                    <li>All Products</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Kids</li>
+                    <li>Blogs</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
 		</div>
 	);
 };
