3b0a3ac245ee0bdaa55cd06beb58e9eb30927812
Web Developer <juniorwebdeveloper.mtkn@gmail.com>
2024-11-26 14:19:28 +0630

    fixed incorrect filename

--- src/Pages/users/Home.jsx
+++ src/Pages/users/Home.jsx
@@ -1,8 +1,32 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 const Home = () => {
-  return (
-    <div className="content-center text-center h-screen">Home</div>
-  )
-}
+	var settings = {
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+    autoplay: true
+	};
 
-export default Home
+	return (
+		<div className="content-center text-center w-full h-screen bg-slate-400">
+			<Slider {...settings}>
+				<div className="w-full h-screen bg-red-300">
+					<h3>1</h3>
+				</div>
+				<div className="w-full h-screen  bg-sky-400">
+					<h3>2</h3>
+				</div>
+				<div className="w-full h-screen bg-cyan-300">
+					<h3>3</h3>
+				</div>
+			</Slider>
+		</div>
+	);
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,48 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import './index.css';
-import PageNotFound from './Pages/PageNotFound';
-import { UserRoutes } from './routes/userRoutes';
-
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserRoutes } from "./routes/UserRoutes";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      {UserRoutes.map(({ path, element, children }) => (
-      <Route key={path} path={path} element={element}>
-        {children?.map(({ path: childPath, element: childElement }) => (
-          <Route key={childPath} path={childPath} element={childElement} />
-        ))}
-      </Route>
-    ))}
-    <Route path='*' element={<PageNotFound />} /> 
-    </>
-  ),
-  {
-    future: {
-      v7_startTransition: true,
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    },
-  }
+	createRoutesFromElements(
+		<>
+			{UserRoutes.map(({ path, element, children }) => (
+				<Route key={path} path={path} element={element}>
+					{children?.map(
+						({ path: childPath, element: childElement }) => (
+							<Route
+								key={childPath}
+								path={childPath}
+								element={childElement}
+							/>
+						)
+					)}
+				</Route>
+			))}
+			<Route path="*" element={<PageNotFound />} />
+		</>
+	),
+	{
+		future: {
+			v7_startTransition: true,
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionErrorRevalidation: true,
+		},
+	}
 );
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
 );
